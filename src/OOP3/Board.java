170410e894d16c0d6815af8d7296b7ab0e0b46bd
Mattias Seth <mattias.seth@gmail.com>
2023-10-27 11:52:12 +0200

    Start

---
+++ src/OOP3/Board.java
@@ -0,0 +1,15 @@
+package OOP3;
+
+import javax.swing.*;
+
+public class Board extends JFrame {
+
+    public Board (){
+
+    }
+
+    public static void main(String[] args) {
+
+        Board b = new Board();
+    }
+}
