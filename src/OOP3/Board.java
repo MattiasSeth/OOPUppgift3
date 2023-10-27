ef76a4a4cfaa44b783f4f8ef0819ae5e33d9f17e
Mattias Seth <mattias.seth@gmail.com>
2023-10-27 14:39:28 +0200

    JButtons changed to a 4x4 matrix and added a method to create 16buttons.

--- src/OOP3/Board.java
+++ src/OOP3/Board.java
@@ -6,49 +6,29 @@ import java.awt.*;
 public class Board extends JFrame {
 
     JPanel boardPanel = new JPanel();
-    JButton b1 = new JButton();
-    JButton b2 = new JButton();
-    JButton b3 = new JButton();
-    JButton b4 = new JButton();
-    JButton b5 = new JButton();
-    JButton b6 = new JButton();
-    JButton b7 = new JButton();
-    JButton b8 = new JButton();
-    JButton b9 = new JButton();
-    JButton b10 = new JButton();
-    JButton b11 = new JButton();
-    JButton b12 = new JButton();
-    JButton b13 = new JButton();
-    JButton b14 = new JButton();
-    JButton b15 = new JButton();
-    JButton b16 = new JButton();
+    JButton[][] buttons = new JButton[4][4];
+
 
 
     public Board() {
         setSize(400,400);
         this.add(boardPanel);
         boardPanel.setLayout(new GridLayout(4,4));
-        boardPanel.add(b1);
-        boardPanel.add(b2);
-        boardPanel.add(b3);
-        boardPanel.add(b4);
-        boardPanel.add(b5);
-        boardPanel.add(b6);
-        boardPanel.add(b7);
-        boardPanel.add(b8);
-        boardPanel.add(b9);
-        boardPanel.add(b10);
-        boardPanel.add(b11);
-        boardPanel.add(b12);
-        boardPanel.add(b13);
-        boardPanel.add(b14);
-        boardPanel.add(b15);
-        boardPanel.add(b16);
+
+        setupBoard();
 
 
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public void setupBoard() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                buttons[i][j] = new JButton();
+                boardPanel.add(buttons[i][j]);
+            }
+        }
     }
 
     public static void main(String[] args) {
