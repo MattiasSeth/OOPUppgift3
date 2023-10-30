47160bd850e23ec3d5713ded4122c1a775b7ede5
Mattias Seth <mattias.seth@gmail.com>
2023-10-30 19:40:24 +0100

    Removed matrix2 from board

--- src/OOP3/Board.java
+++ src/OOP3/Board.java
@@ -17,11 +17,7 @@ public class Board extends JFrame {
     public int[][] matrix;
 
     public Board() {
-        // Check win matrix and empty matrix
-        int[][] matrix2 = {  { 1, 2, 3, 4 },
-                             { 5, 6, 7, 8 },
-                             { 9, 10, 11, 12 },
-                             { 13, 14, 0, 15 } };
+        // empty matrix
         matrix = new int[4][4];
 
 
